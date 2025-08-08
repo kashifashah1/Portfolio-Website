691df9b67664960c8e6fcdc554a62d740408be76
kashifashah1 <kashifa.shah24@gmail.com>
2025-08-08 19:07:46 +0530

    Added some Mobile Responsiveness

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import About from './components/About/About'
-import Services from './components/Services/Services'
-import MyWork from './components/MyWork/MyWork'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import MyWork from "./components/MyWork/MyWork";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Services/>
-      <MyWork/>
-      <Contact/>
-      <Footer/>
+    <div className="w-full overflow-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <MyWork />
+      <Contact />
+      <Footer />
+      {/* Toast container for showing notifications */}
+      <ToastContainer position="top-right" autoClose={3000} theme="colored" />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -1,49 +1,88 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
+import {  toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import theme_pattern from "../../assets/theme_pattern.svg";
 import mail_icon from "../../assets/mail_icon.svg";
 import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "e22368dc-1941-441f-b98b-8724efb98c46");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      toast.success("Form Submitted Successfully!");
+      event.target.reset();
+    } else {
+      toast.error(res.message || "Something went wrong.");
+    }
+  };
+
   return (
-    <div id='contact' className='contact'>
+    <div id="contact" className="contact">
       <div className="contact-title">
         <h1>Get in touch</h1>
         <img src={theme_pattern} alt="" />
       </div>
       <div className="contact-section">
         <div className="contact-left">
-            <h1>Let's talk</h1>
-            <p>I'm currently available to take new projects, so feel free to send me a message about anything that you want me to work on. You can contact me anytime.</p>
-            <div className="contact-details">
-                <div className="contact-detail">
-                    <img src={mail_icon} alt="" />
-                    <p>kashifa.shah24@gmail.com</p>
-                </div>
-                <div className="contact-detail">
-                    <img src={call_icon} alt="" />
-                    <p>+777-825-524</p>
-                </div>
-                <div className="contact-detail">
-                    <img src={location_icon} alt="" />
-                    <p>Mandsaur, M.P</p>
-                </div>
+          <h1>Let's talk</h1>
+          <p>
+            I'm currently available to take new projects, so feel free to send
+            me a message about anything that you want me to work on. You can
+            contact me anytime.
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>kashifa.shah24@gmail.com</p>
             </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" />
+              <p>+777-825-524</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" />
+              <p>Mandsaur, M.P</p>
+            </div>
+          </div>
         </div>
-        <form  className="contact-right">
-            <label htmlFor="">Your Name</label>
-            <input type="text" placeholder="Enter your name" name="name" />
-            <label htmlFor="">Your Email</label>
-            <input type="text" placeholder="Enter your name" name="email" />
-            <label htmlFor="">Write your message here</label>
-            <textarea name="message" rows="8" placeholder='
-            Enter your message'></textarea>
-            <button type='submit' className='contact-submit'>Submit now</button>
+        <form onSubmit={onSubmit} className="contact-right">
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter your name" name="name" required />
+          <label htmlFor="">Your Email</label>
+          <input type="text" placeholder="Enter your name" name="email" required />
+          <label htmlFor="">Write your message here</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="
+            Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="contact-submit">
+            Submit now
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
